a92c3731c13948e60eddd239a4eafe02463ad1f1
Anton <asmeiko13@gmail.com>
2023-11-27 14:18:49 +0100

    update

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const categories = document.querySelector('#categories');
+console.log(`Number of categories: ${categories.childElementCount}`);
+
+const item = document.querySelector('.item');
+h2Elements = item.querySelectorAll('h2');
+console.log(h2Elements);
