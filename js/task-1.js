347ef18fcbe923f9433a2b92dde69b8fd842929e
Anton <131876090+NE1LO@users.noreply.github.com>
2023-12-01 10:19:19 +0100

    update

--- js/task-1.js
+++ js/task-1.js
@@ -1,6 +1,14 @@
-const categories = document.querySelector('#categories');
+const categories = document.querySelector("#categories");
 console.log(`Number of categories: ${categories.childElementCount}`);
 
-const item = document.querySelector('.item');
-h2Elements = item.querySelectorAll('h2');
-console.log(h2Elements);
+const items = document.querySelectorAll(".item");
+
+items.forEach((item) => {
+  const h2Element = item.querySelector("h2");
+  const categoryName = h2Element.textContent;
+  const categoryItemsCount = item.querySelectorAll("ul li").length;
+
+  console.log(`Category: ${categoryName}`);
+  console.log(`Number of items: ${categoryItemsCount}`);
+  console.log("------------------------");
+});
