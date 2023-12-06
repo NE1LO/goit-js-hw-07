7a16b4a6a5b8fd2f84b8017c905596244a5c4d77
Anton <131876090+NE1LO@users.noreply.github.com>
2023-12-06 13:17:38 +0100

    update

---
+++ js/task-6.js
@@ -0,0 +1,37 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  const boxesContainer = document.querySelector("#boxes");
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${30 + i * 10}px`;
+    box.style.height = `${30 + i * 10}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+}
+
+function destroyBoxes() {
+  const boxesContainer = document.querySelector("#boxes");
+  boxesContainer.innerHTML = "";
+}
+
+document.querySelector("[data-create]").addEventListener("click", function () {
+  const input = document.querySelector("input");
+  const amount = parseInt(input.value, 10);
+
+  if (amount >= 1 && amount <= 100) {
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    return;
+  }
+});
+
+document
+  .querySelector("[data-destroy]")
+  .addEventListener("click", destroyBoxes);
